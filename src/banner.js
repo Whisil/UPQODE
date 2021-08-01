9262f856652a8b2c482abd8d8923d42b47b82c97
David <electromandarin@gmail.com>
2021-08-01 13:07:39 +0300

    adaptive done

---
+++ src/banner.js
@@ -0,0 +1,14 @@
+// window.addEventListener('scroll', () => {
+
+//         if(pageYOffset >= innerHeight * 0.5){
+            
+            
+
+//         }
+// });
+
+// $('a.stop-video').click(function(){
+//     $('.youtube-video')[0].contentWindow.postMessage('{"event":"command","func":"' + 'stopVideo' + '","args":""}', '*');
+//   });
+
+
