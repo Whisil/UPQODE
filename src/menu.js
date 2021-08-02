a7ae9f74d65d6b6e359caa4660cfbd6fbf90a267
David <electromandarin@gmail.com>
2021-08-02 21:19:45 +0300

    minor changes

--- src/menu.js
+++ src/menu.js
@@ -1,10 +1,10 @@
 const header = document.querySelector('header');
 
 window.addEventListener('scroll', () => {
-    if(pageYOffset >= innerHeight * 0.5){
+    if(pageYOffset >= innerHeight * 0.1){
         header.style.backgroundColor = "#222222";
         header.style.paddingTop = '20px';
-    } else if(pageYOffset < innerHeight * 0.5){
+    } else if(pageYOffset < innerHeight * 0.1){
         header.style.backgroundColor = 'transparent';
         header.style.paddingTop = '27px'
     }
